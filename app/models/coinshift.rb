5616c020f8e5eba25ffa3d8728c0eedb60d093f2
Tiago Amaro <tiagocis@gmail.com>
2014-03-15 15:07:36 -0300

    caching api calls

--- app/models/coinshift.rb
+++ app/models/coinshift.rb
@@ -4,11 +4,26 @@ class Coinshift
 
   COINSHIFT_URL = 'http://coinshift.com'
   MONTH_PERFORMANCE_URL = "#{COINSHIFT_URL}/graph/api/v1/performance/30d"
-  ADDRESS_URL = 'http://coinshift.com/api/stats/address/:payout_address'
+  ADDRESS_URL = 'http://coinshift.com/api/stats/address/:address'
 
   def self.month_performance
-    json = JSON.parse(open(MONTH_PERFORMANCE_URL).read)
+    json = Rails.cache.fetch(['month_performance'], expires_in: 10.minutes) do
+      JSON.parse(open(MONTH_PERFORMANCE_URL).read)
+    end
 
+    transform_month_performance(json) unless json.empty?
+  end
+
+  def self.address_data(address)
+    json = Rails.cache.fetch(['address', address], expires_in: 10.minutes) do
+      address_url = ADDRESS_URL.gsub(':address', address)
+      JSON.parse(open(address_url).read)
+    end
+  end
+
+  private
+
+  def self.transform_month_performance(json)
     btc_per_mh = reverse_datapoints json[0]['datapoints']
     hashrate = reverse_datapoints json[1]['datapoints']
     reject = reverse_datapoints json[2]['datapoints']
