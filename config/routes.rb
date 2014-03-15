bdcc7852582b36c974bb80105de8bb1f5df08cee
Tiago Amaro <tiagocis@gmail.com>
2014-03-15 19:14:21 -0300

    changing faq to about

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Statting::Application.routes.draw do
 
   get '/wallet', to: 'wallet#address', as: 'wallet_address'
   get '/address_data/:btc_address', to: 'wallet#address_data'
-  get '/faq', to: 'wallet#faq'
+  get '/about', to: 'wallet#about'
 
   get '/coinshift/month_performance', to: 'coinshift#month_performance'
 
