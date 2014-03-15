7317666b0df296c464de1303b526df09a3eff405
Tiago Amaro <tiagocis@gmail.com>
2014-03-15 16:07:37 -0300

    create address on database if it does not exist

--- app/controllers/wallet_controller.rb
+++ app/controllers/wallet_controller.rb
@@ -1,8 +1,10 @@
 class WalletController < ApplicationController
   def address
-    @btc_address = params[:btc_address]
+    btc_address = params[:btc_address]
 
-    unless Bitcoin::valid_address?(@btc_address)
+    if Bitcoin::valid_address?(btc_address)
+      @btc_address = Address.find_or_create_by(address: btc_address)
+    else
       flash[:warning] = 'Invalid BTC Address'
       redirect_to root_path
     end
