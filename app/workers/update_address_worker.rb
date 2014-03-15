37f246e5a956e39121f018b668662597c5813833
Tiago Amaro <tiagocis@gmail.com>
2014-03-15 16:17:31 -0300

    worker that update all addresses (WIP)

---
+++ app/workers/update_address_worker.rb
@@ -0,0 +1,18 @@
+class UpdateAddressWorker
+  include Sidekiq::Worker
+
+  # TODO: If we have my addresses, this will not work well
+  def perform
+    Address.all.each do |btc_address|
+      json = Coinshift.address_data(btc_address.address)
+
+      btc_address.payouts.destroy_all
+
+      Balance.create(exchanged: json['exchanged_balance'], unexchanged: json['unexchanged_balance'], address: btc_address)
+      HashRate.create(hash_rate: json['hashrate'], reject_rate: json['rejectrate'], address: btc_address)
+      json['payouts'].each do |payout|
+        Payout.create(timestamp: payout['timestamp'], value: payout['value'], tx: payout['tx'], address: btc_address)
+      end
+    end
+  end
+end
